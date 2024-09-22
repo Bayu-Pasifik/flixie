884bda62a62d734503fd41ca5a4527a9003ae398
milky_way <shin.kagami13@gmail.com>
2024-09-22 22:37:08 +0700

    feat: create videos hook

---
+++ src/hooks/useVideo.ts
@@ -0,0 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
+import axiosInstance from '../lib/axios';
+import { Videos } from '@/types/videoType';
+
+// Mengambil detail movie berdasarkan ID
+async function fetchVideos(id: number): Promise<Videos> {
+  try {
+    const response = await axiosInstance.get(`/movie/${id}/videos`);
+    return response.data; // Asumsi response data berupa object Detailmovie
+  } catch (error) {
+    console.error('Error fetching movie videos:', error);
+    throw error;
+  }
+}
+
+// Hook untuk menggunakan React Query
+export const useVideos = (id: number) => {
+  return useQuery<Videos, Error>({
+    queryKey: ['movieDetail', id],
+    queryFn: () => fetchVideos(id),
+    staleTime: 1000 * 60 * 5, // 5 menit
+  });
+};
--- src/types/videoType.ts
+++ src/types/videoType.ts
@@ -1,4 +1,4 @@
-export type Video = {
+export type Videos = {
     iso_639_1:    string;
     iso_3166_1:   string;
     name:         string;
