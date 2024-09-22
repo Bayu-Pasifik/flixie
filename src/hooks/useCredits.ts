8299250a8742d0645a66cd774c0828a6ce0b3ddf
milky_way <shin.kagami13@gmail.com>
2024-09-22 22:32:50 +0700

    feat: make a credits hook

---
+++ src/hooks/useCredits.ts
@@ -0,0 +1,24 @@
+'use server';
+import { useQuery } from '@tanstack/react-query';
+import axiosInstance from '../lib/axios';
+import {Credit} from '@/types/personType'
+
+// Mengambil detail movie berdasarkan ID
+async function fetchCredits(id: number): Promise<Credit> {
+  try {
+    const response = await axiosInstance.get(`/movie/${id}/credits`);
+    return response.data; // Asumsi response data berupa object Detailmovie
+  } catch (error) {
+    console.error('Error fetching movie detail:', error);
+    throw error;
+  }
+}
+
+// Hook untuk menggunakan React Query
+export const useCredits = (id: number) => {
+  return useQuery<Credit, Error>({
+    queryKey: ['movies/credits', id],
+    queryFn: () => fetchCredits(id),
+    staleTime: 1000 * 60 * 5, // 5 menit
+  });
+};
