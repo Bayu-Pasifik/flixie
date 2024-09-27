c8d7de45d2baf4c2a909f6108835c9cbc717e4b9
milky_way <shin.kagami13@gmail.com>
2024-09-28 00:45:22 +0700

    feat: create page for see all poster images

---
+++ src/app/movie/[id]/posters/page.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { useImages } from "@/hooks/useImages";
+import { useParams } from "next/navigation";
+import { LayoutTemplate } from "@/components/LayoutTemplate";
+import LoadingIndicator from "@/components/LoadingIndicator";
+import { useState } from "react";
+import { ImageModal } from "@/components/ImageModal";
+import { MotionCard } from "@/components/MotionCard";
+
+export default function ImagesPage() {
+  const { id } = useParams();
+  const movieId = typeof id === "string" ? parseInt(id, 10) : 0;
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  const handleImageClick = (imagePath: string) => {
+    setSelectedImage(
+      `${process.env.NEXT_PUBLIC_IMAGE_URL_ORIGINAL}${imagePath}`
+    );
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+  };
+
+  const {
+    data: images,
+    isLoading: imagesLoading,
+    error: imagesError,
+  } = useImages(movieId);
+
+  if (imagesLoading) return <LoadingIndicator />;
+  if (imagesError) return <div>Error: {imagesError.message}</div>;
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">All Posters Images</h1>
+
+      <LayoutTemplate layout="card">
+        {images?.posters.map((image, index) => (
+          <MotionCard
+           type="poster"
+            key={image.file_path}
+            imageSrc={process.env.NEXT_PUBLIC_IMAGE_URL + image.file_path}
+            index={index}
+            onClick={() => handleImageClick(image.file_path)} // Menambahkan onClick ke MotionCard
+          />
+        ))}
+      </LayoutTemplate>
+
+      {/* Image Modal */}
+      {selectedImage && (
+        <ImageModal
+          imageUrl={selectedImage}
+          onClose={handleCloseModal}
+          isOpen={!!selectedImage}
+        />
+      )}
+    </div>
+  );
+}
--- src/components/MotionCard.tsx
+++ src/components/MotionCard.tsx
@@ -4,9 +4,13 @@ interface MotionCardProps {
   imageSrc: string;
   index: number;
   onClick: () => void; // Menambahkan prop onClick
+  type?: string; // Menambahkan prop type
 }
 
-export const MotionCard: React.FC<MotionCardProps> = ({ imageSrc, index, onClick }) => {
+export const MotionCard: React.FC<MotionCardProps> = ({ imageSrc, index, onClick, type }) => {
+  // Mengatur tinggi berdasarkan type
+  const heightClass = type === 'poster' ? '' : 'h-64'; // Misalkan 'h-96' adalah tinggi untuk poster
+
   return (
     <motion.div
       onClick={onClick} // Menambahkan event onClick
@@ -29,7 +33,7 @@ export const MotionCard: React.FC<MotionCardProps> = ({ imageSrc, index, onClick
         type: "spring", // Tipe animasi yang lebih halus
         stiffness: 100, // Kekakuan pegas
       }}
-      className="w-full h-60 object-cover mb-4 rounded-lg overflow-hidden cursor-pointer" // Menambahkan cursor pointer
+      className={`w-full ${heightClass} object-cover mb-4 rounded-lg overflow-hidden cursor-pointer`} // Menambahkan cursor pointer
     >
       <img
         src={imageSrc}
