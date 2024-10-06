fd91552c6a15d1492b2448f95e21873d3f802a39
milky_way <shin.kagami13@gmail.com>
2024-10-07 00:26:02 +0700

    feat: make a DetailCompanyTvShowPage UI + Logic base

---
+++ src/app/company/[id]/tvshows/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { LayoutTemplate } from "@/components/LayoutTemplate";
+import MovieCard from "@/components/MovieCard";
+import MovieListCard from "@/components/MovieListCard";
+import { useDetailCompany, useInfinityTvByCompany } from "@/hooks/useCompany";
+import { useParams } from "next/navigation";
+import React from "react";
+
+export default function DetailCompanyTvShowPage() {
+  const { id } = useParams();
+  const companyId = typeof id === "string" ? parseInt(id, 10) : 0;
+  const { data: company } = useDetailCompany(companyId);
+  const { data: tvshows } = useInfinityTvByCompany(companyId);
+  return (
+    <div className="p-4  text-white">
+      <h1 className="text-2xl font-bold mb-4">
+        TV Shows that {company?.name} produced
+      </h1>
+      <LayoutTemplate layout="card">
+        {tvshows?.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.tvShows.map((tvShow, idx) => (
+              <MovieCard
+                id={tvShow.id}
+                key={idx}
+                title={tvShow.name}
+                overview={tvShow.overview}
+                posterPath={tvShow.poster_path}
+                type="tv"
+              />
+            ))}
+          </React.Fragment>
+        ))}
+      </LayoutTemplate>
+    </div>
+  );
+}
