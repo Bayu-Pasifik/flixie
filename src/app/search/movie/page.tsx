c6393efe9f7c6a4ee7bac54f075edefa65d2c56e
milky_way <shin.kagami13@gmail.com>
2024-10-12 01:25:03 +0700

    fix: Fixing add advanced search button to hide and show advanced search form

--- src/app/search/movie/page.tsx
+++ src/app/search/movie/page.tsx
@@ -26,7 +26,6 @@ const generateRatingOptions = () => {
   return options;
 };
 
-// Define the type for OptionType explicitly
 type OptionType = {
   value: string;
   label: string;
@@ -37,6 +36,7 @@ export default function SearchMoviePage() {
   const params = useSearchParams();
   const initialQuery = params.get("query");
   const [query, setQuery] = useState<string>(initialQuery || "");
+  const [showAdvancedSearch, setShowAdvancedSearch] = useState(false);
 
   // State for filtering
   const [runtimeMoreThan, setRuntimeMoreThan] = useState<number>(0);
@@ -45,14 +45,12 @@ export default function SearchMoviePage() {
   const [ratingLessThan, setRatingLessThan] = useState<number>(0);
   const [year, setYear] = useState<number>(0);
 
-  // Add types to all filter states
   const [companyId, setCompanyId] = useState<OptionType[]>([]);
   const [genresId, setGenresId] = useState<OptionType[]>([]);
   const [keywordsId, setKeywordsId] = useState<OptionType[]>([]);
   const [countryId, setCountryId] = useState<OptionType | null>(null);
   const [languagesId, setLanguagesId] = useState<OptionType[]>([]);
 
-  // Active filters state
   const [activeFilters, setActiveFilters] = useState({
     runtimeMoreThan: 0,
     runtimeLessThan: 0,
@@ -66,7 +64,6 @@ export default function SearchMoviePage() {
     languagesId: [] as OptionType[],
   });
 
-  // Fetch data for dropdowns
   const { companies, keywords } = useAdvancedSearchData();
   const { data: genres } = useMovieGenre();
   const { data: countries } = useCountries();
@@ -130,7 +127,11 @@ export default function SearchMoviePage() {
     return () => clearTimeout(delayDebounceFn);
   }, [query]);
 
-  const handleSearch = () => {
+  const handleShowAdvancedSearch = () => {
+    setShowAdvancedSearch((prev) => !prev);
+  };
+
+  const handleApplyFilters = () => {
     setUseAdvancedSearch(true);
     setQuery("");
     setActiveFilters({
@@ -157,155 +158,168 @@ export default function SearchMoviePage() {
         placeholder="Search for movies..."
         className="p-2 border rounded-md mb-4 text-black w-full"
       />
-      {/* Advanced Search Filters */}
-      <div className="mb-4 grid grid-cols-1 md:grid-cols-2 gap-4">
-        {/* Example filter component */}
-        <div>
-          <label>Runtime More Than:</label>
-          <input
-            type="text"
-            value={runtimeMoreThan}
-            onChange={(e) => setRuntimeMoreThan(Number(e.target.value) || 0)}
-            placeholder="e.g. 90"
-            className="p-2 border rounded-md mb-2 text-black"
-          />
-        </div>
-        {/* Add other filter inputs here */}
-
-        {/* Filters for Runtime, Rating, and Year */}
-        <div>
-          <label>Runtime More Than:</label>
-          <input
-            type="text"
-            value={runtimeMoreThan}
-            onChange={(e) => setRuntimeMoreThan(Number(e.target.value) || 0)}
-            placeholder="e.g. 90"
-            className="p-2 border rounded-md mb-2 text-black"
-          />
-        </div>
-        <div>
-          <label>Runtime Less Than:</label>
-          <input
-            type="text"
-            value={runtimeLessThan}
-            onChange={(e) => setRuntimeLessThan(Number(e.target.value) || 0)}
-            placeholder="e.g. 150"
-            className="p-2 border rounded-md mb-2 text-black"
-          />
-        </div>
-        <div>
-          <label>Rating More Than:</label>
-          <Select
-            options={generateRatingOptions()}
-            value={
-              ratingMoreThan > 0 // Ensure the value is only set if greater than 0
-                ? { value: ratingMoreThan, label: ratingMoreThan.toString() }
-                : null
-            }
-            onChange={(option) => setRatingMoreThan(option ? option.value : 0)}
-            placeholder="Select rating..."
-            className="mb-2 text-black"
-          />
-        </div>
-        <div>
-          <label>Rating Less Than:</label>
-          <Select
-            options={generateRatingOptions()}
-            value={
-              ratingLessThan > 0 // Ensure the value is only set if greater than 0
-                ? { value: ratingLessThan, label: ratingLessThan.toString() }
-                : null
-            }
-            onChange={(option) => setRatingLessThan(option ? option.value : 0)}
-            placeholder="Select rating..."
-            className="mb-2 text-black"
-          />
-        </div>
-        <div>
-          <label>Year:</label>
-          <input
-            type="text"
-            value={year}
-            onChange={(e) => setYear(Number(e.target.value) || 0)}
-            placeholder="e.g. 2023"
-            className="p-2 border rounded-md mb-2 text-black"
-          />
-        </div>
-        {/* Select Companies */}
-        <div>
-          <label>Companies:</label>
-          <AsyncSelect
-            loadOptions={loadCompanyOptions}
-            isClearable
-            isMulti
-            placeholder="Select company..."
-            onChange={(value) => setCompanyId(value as OptionType[])}
-            className="mb-2 text-black"
-          />
-        </div>
-        {/* Select Genres */}
-        <div>
-          <label>Genres:</label>
-          <Select
-            options={genres?.map((genre) => ({
-              value: genre.id.toString(),
-              label: genre.name,
-            }))}
-            isClearable
-            isMulti
-            placeholder="Select genres..."
-            onChange={(value) => setGenresId(value as OptionType[])}
-            className="mb-2 text-black"
-          />
-        </div>
-        {/* Select Keywords */}
-        <div>
-          <label>Keywords:</label>
-          <AsyncSelect
-            loadOptions={loadKeywordOptions}
-            isClearable
-            isMulti
-            placeholder="Select keywords..."
-            onChange={(value) => setKeywordsId(value as OptionType[])}
-            className="mb-2 text-black"
-          />
-        </div>
-        {/* Select Countries */}
-        <div>
-          <label>Countries:</label>
-          <Select
-            options={countries?.map((country) => ({
-              value: country.iso_3166_1,
-              label: country.english_name,
-            }))}
-            isClearable
-            placeholder="Select country..." // Updated to singular
-            onChange={(value) => setCountryId(value as OptionType | null)} // Allow single selection
-            className="mb-2 text-black"
-          />
-        </div>
-        {/* Select Languages */}
-        <div>
-          <label>Languages:</label>
-          <Select
-            options={languages?.map((language) => ({
-              value: language.iso_639_1,
-              label: language.english_name,
-            }))}
-            isClearable
-            isMulti
-            placeholder="Select languages..."
-            onChange={(value) => setLanguagesId(value as OptionType[])}
-            className="mb-2 text-black"
-          />
-        </div>
-      </div>
+      {/* Show Advanced Search Button */}
       <button
-        onClick={handleSearch}
-        className="bg-blue-500 text-white p-2 rounded-md mb-4"
+        onClick={handleShowAdvancedSearch}
+        className="bg-gray-500 text-white p-2 rounded-md mb-4"
       >
-        Apply Filters
+        {showAdvancedSearch ? "Hide Advanced Search" : "Show Advanced Search"}
       </button>
+      {/* Advanced Search Filters */}
+      {showAdvancedSearch && (
+        <>
+          <div className="mb-4 grid grid-cols-1 md:grid-cols-2 gap-4">
+            {/* Filters for Runtime, Rating, and Year */}
+            <div>
+              <label>Runtime More Than:</label>
+              <input
+                type="text"
+                value={runtimeMoreThan}
+                onChange={(e) =>
+                  setRuntimeMoreThan(Number(e.target.value) || 0)
+                }
+                placeholder="e.g. 90"
+                className="p-2 border rounded-md mb-2 text-black ml-3"
+              />
+            </div>
+            <div>
+              <label>Runtime Less Than:</label>
+              <input
+                type="text"
+                value={runtimeLessThan}
+                onChange={(e) =>
+                  setRuntimeLessThan(Number(e.target.value) || 0)
+                }
+                placeholder="e.g. 150"
+                className="p-2 border rounded-md mb-2 text-black ml-3"
+              />
+            </div>
+            <div>
+              <label>Rating More Than:</label>
+              <Select
+                options={generateRatingOptions()}
+                value={
+                  ratingMoreThan > 0 // Ensure the value is only set if greater than 0
+                    ? {
+                        value: ratingMoreThan,
+                        label: ratingMoreThan.toString(),
+                      }
+                    : null
+                }
+                onChange={(option) =>
+                  setRatingMoreThan(option ? option.value : 0)
+                }
+                placeholder="Select rating..."
+                className="mb-2 text-black mt-2"
+              />
+            </div>
+            <div>
+              <label>Rating Less Than:</label>
+              <Select
+                options={generateRatingOptions()}
+                value={
+                  ratingLessThan > 0 // Ensure the value is only set if greater than 0
+                    ? {
+                        value: ratingLessThan,
+                        label: ratingLessThan.toString(),
+                      }
+                    : null
+                }
+                onChange={(option) =>
+                  setRatingLessThan(option ? option.value : 0)
+                }
+                placeholder="Select rating..."
+                className="mb-2 text-black mt-2"
+              />
+            </div>
+            <div>
+              <label>Year:</label>
+              <input
+                type="text"
+                value={year}
+                onChange={(e) => setYear(Number(e.target.value) || 0)}
+                placeholder="e.g. 2023"
+                className="p-2 border rounded-md mb-2 text-black ml-4"
+              />
+            </div>
+            {/* Select Companies */}
+            <div>
+              <label>Companies:</label>
+              <AsyncSelect
+                loadOptions={loadCompanyOptions}
+                isClearable
+                isMulti
+                placeholder="Select company..."
+                onChange={(value) => setCompanyId(value as OptionType[])}
+                className="mb-2 text-black mt-2"
+              />
+            </div>
+            {/* Select Genres */}
+            <div>
+              <label>Genres:</label>
+              <Select
+                options={genres?.map((genre) => ({
+                  value: genre.id.toString(),
+                  label: genre.name,
+                }))}
+                isClearable
+                isMulti
+                placeholder="Select genres..."
+                onChange={(value) => setGenresId(value as OptionType[])}
+                className="mb-2 text-black mt-2"
+              />
+            </div>
+            {/* Select Keywords */}
+            <div>
+              <label>Keywords:</label>
+              <AsyncSelect
+                loadOptions={loadKeywordOptions}
+                isClearable
+                isMulti
+                placeholder="Select keywords..."
+                onChange={(value) => setKeywordsId(value as OptionType[])}
+                className="mb-2 text-black mt-2"
+              />
+            </div>
+            {/* Select Countries */}
+            <div>
+              <label>Countries:</label>
+              <Select
+                options={countries?.map((country) => ({
+                  value: country.iso_3166_1,
+                  label: country.english_name,
+                }))}
+                isClearable
+                placeholder="Select country..." // Updated to singular
+                onChange={(value) => setCountryId(value as OptionType | null)} // Allow single selection
+                className="mb-2 text-black mt-2"
+              />
+            </div>
+            {/* Select Languages */}
+            <div>
+              <label>Languages:</label>
+              <Select
+                options={languages?.map((language) => ({
+                  value: language.iso_639_1,
+                  label: language.english_name,
+                }))}
+                isClearable
+                isMulti
+                placeholder="Select languages..."
+                onChange={(value) => setLanguagesId(value as OptionType[])}
+                className="mb-2 text-black mt-2"
+              />
+            </div>
+          </div>
+          {/* Apply Filters Button */}
+          <button
+            onClick={handleApplyFilters}
+            className="bg-blue-500 text-white p-2 w-full rounded-md mb-4"
+          >
+            Apply Filters
+          </button>
+        </>
+      )}
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
         {(useAdvancedSearch ? movieData : searchData)?.pages.flatMap((page) =>
           page.movies.map((movie) => (
